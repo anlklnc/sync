998de3b5d24a658aa9055edcdb4d53e6a9fae7e5
anlklnc <anilklnc@gmail.com>
2017-10-25 17:57:11 +0300

    durumlar otomatik takip ediliyor

--- src/main/java/com/nutomic/syncthingandroid/kife/KifeActivity.java
+++ src/main/java/com/nutomic/syncthingandroid/kife/KifeActivity.java
@@ -3,6 +3,8 @@ package com.nutomic.syncthingandroid.kife;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -15,17 +17,20 @@ import com.nutomic.syncthingandroid.model.Connections;
 import com.nutomic.syncthingandroid.model.Device;
 import com.nutomic.syncthingandroid.model.Folder;
 import com.nutomic.syncthingandroid.model.Model;
-import com.nutomic.syncthingandroid.model.SystemInfo;
 import com.nutomic.syncthingandroid.service.SyncthingService;
 import com.nutomic.syncthingandroid.util.Util;
 
-import java.text.NumberFormat;
 import java.util.ArrayList;
 
-public class KifeActivity extends SyncthingActivity implements SyncthingService.OnApiChangeListener{
+public class KifeActivity extends SyncthingActivity{
 
-    String deviceId, cpuUsage, download, upload;
-    TextView twId, twLabel, twPath, twPercentage, twItems, twSize, twState;
+    static final int UPDATE_TIME = 2000;
+    boolean isPolling = false;
+    boolean indicator = true;
+    int counter = 0;
+
+    TextView twId, twLabel, twPath, twPercentage, twItems, twSize, twState, twTotal, twUp, twDown;
+    View indicatorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,44 +44,48 @@ public class KifeActivity extends SyncthingActivity implements SyncthingService.
         twItems = (TextView) findViewById(R.id.tw_items);
         twSize = (TextView) findViewById(R.id.tw_size);
         twState = (TextView) findViewById(R.id.tw_state);
-    }
-
-    @Override
-    public void onApiChange(SyncthingService.State currentState) {
-        update();
-    }
-
-    void update() {
-        getApi().getSystemInfo(this::onReceiveSystemInfo);
-        getApi().getConnections(this::onReceiveConnections);
-    }
-
-    /** Sistem bilgileri buraya geliyor. */
-    public void onReceiveSystemInfo(SystemInfo info) {
-        deviceId = info.myID;
-
-        NumberFormat percentFormat = NumberFormat.getPercentInstance();
-        percentFormat.setMaximumFractionDigits(2);
-        cpuUsage = percentFormat.format(info.cpuPercent / 100);
-
-        Log.i("!!!", "Device id: " + deviceId);
-        Log.i("!!!", "Cpu usage: " + cpuUsage);
-    }
-
-    /** Bağlantı bilgileri buraya geliyor. */
-    private void onReceiveConnections(Connections connections) {
-        Connections.Connection c = connections.total;
-        download = Util.readableTransferRate(this, c.inBits);
-        upload = Util.readableTransferRate(this, c.outBits);
-
-        Log.i("!!!", "Download: " + download);
-        Log.i("!!!", "Upload: " + upload);
+        twTotal = (TextView) findViewById(R.id.tw_total);
+        twUp = (TextView) findViewById(R.id.tw_up);
+        twDown = (TextView) findViewById(R.id.tw_down);
+        indicatorView = findViewById(R.id.indicator);
     }
 
     public void restartService(View view) {
         Intent intent = new Intent(this, SyncthingService.class);
         intent.setAction(SyncthingService.ACTION_RESTART);
         this.startService(intent);
+    }
+
+    public void folders(View v) {
+
+        if(isPolling) { //buraya iki defa girmesini engelle
+            return;
+        }
+        isPolling = true;
+
+        //update folder info
+        Folder folder = getApi().getFolders().get(0);   //ilk folderı al
+        String label = TextUtils.isEmpty(folder.label) ? folder.id : folder.label;
+        twId.setText(folder.id);
+        twLabel.setText(label);
+        twPath.setText(folder.path);
+
+        //update sync details
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                getApi().getModel(folder.id, KifeActivity.this::onReceiveModel); //updateFolderInfo'a ait modeli al
+                if(counter == 0) {
+                    connections(null);
+                }
+                counter++;
+                if(counter==3) {
+                    counter = 0;
+                }
+                handler.postDelayed(this, UPDATE_TIME);
+            }
+        }, UPDATE_TIME);
     }
 
     public void devices(View view) {
@@ -89,23 +98,26 @@ public class KifeActivity extends SyncthingActivity implements SyncthingService.
     public void connections(View view) {
         getApi().getConnections(connections -> {
             ArrayList<Device> devices = (ArrayList<Device>) getApi().getDevices(false);
-            String someDeviceId =devices.get(0).deviceID;    //todo device id
+            String someDeviceId =devices.get(0).deviceID;
             Connections.Connection c = connections.connections.get(someDeviceId);
             int completion = -1;  //status by
             try {
                 completion = c.completion;
+                twTotal.setText(completion+"");
             } catch (Exception e) {
                 e.printStackTrace();
             }
             long in = -1;
             try {
                 in = c.inBits;
+                twDown.setText(in+"");
             } catch (Exception e) {
                 e.printStackTrace();
             }
             long out = -1;
             try {
                 out = c.outBits;
+                twUp.setText(out+"");
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -114,20 +126,6 @@ public class KifeActivity extends SyncthingActivity implements SyncthingService.
     }
 
     ///// folderla ilgili kısım
-
-
-
-    public void updateFolderInfo(View view) {
-        Folder folder = getApi().getFolders().get(0);   //ilk folderı al
-        String label = TextUtils.isEmpty(folder.label) ? folder.id : folder.label;
-        String directory = folder.path;
-        getApi().getModel(folder.id, this::onReceiveModel); //updateFolderInfo'a ait modeli al
-        Log.i("!!!", "twId: "+folder.id);
-        Log.i("!!!", "twLabel: "+label);
-        Log.i("!!!", "twPath: "+directory);
-        updateFolderUi1(folder.id, label, directory);
-    }
-
     public void onReceiveModel(String s, Model model) {
 
         int percentage = (model.localBytes != 0)
@@ -139,22 +137,40 @@ public class KifeActivity extends SyncthingActivity implements SyncthingService.
                 Util.readableFileSize(this, model.inSyncBytes),
                 Util.readableFileSize(this, model.localBytes));
 
-        Log.i("!!!", "perc: "+percentage);
-        Log.i("!!!", "twState: "+state);
-        Log.i("!!!", "twItems: "+items);
-        Log.i("!!!", "twSize: "+size);
-        updateFolderUi2(percentage+"", state, items, size);
+//        Log.i("!!!", "perc: "+percentage);
+//        Log.i("!!!", "twState: "+state);
+//        Log.i("!!!", "twItems: "+items);
+//        Log.i("!!!", "twSize: "+size);
+
+        twPercentage.setText(percentage+"");
+        twState.setText(state);
+        twItems.setText(items);
+        twSize.setText(size);
+
+        indicate();
     }
 
-    public static String getLocalizedState(Context c, String state, int percentage) {
+    public String getLocalizedState(Context c, String state, int percentage) {
         switch (state) {
-            case "idle":     return c.getString(R.string.state_idle);
-            case "scanning": return c.getString(R.string.state_scanning);
-            case "cleaning": return c.getString(R.string.state_cleaning);
-            case "syncing":  return c.getString(R.string.state_syncing, percentage);
-            case "error":    return c.getString(R.string.state_error);
+            case "idle":
+                twState.setTextColor(ContextCompat.getColor(this, R.color.state_idle));
+                return c.getString(R.string.state_idle);
+            case "scanning":
+                twState.setTextColor(ContextCompat.getColor(this, R.color.state_scanning));
+                return c.getString(R.string.state_scanning);
+            case "cleaning":
+                twState.setTextColor(ContextCompat.getColor(this, R.color.state_cleaning));
+                return c.getString(R.string.state_cleaning);
+            case "syncing":
+                twState.setTextColor(ContextCompat.getColor(this, R.color.state_syncing));
+                return c.getString(R.string.state_syncing, percentage);
+            case "error":
+                twState.setTextColor(ContextCompat.getColor(this, R.color.state_error));
+                return c.getString(R.string.state_error);
             case "unknown":  // Fallthrough
-            case "":         return c.getString(R.string.state_unknown);
+            case "":
+                twState.setTextColor(ContextCompat.getColor(this, R.color.state_unknown));
+                return c.getString(R.string.state_unknown);
         }
         if (BuildConfig.DEBUG) {
             throw new AssertionError("Unexpected updateFolderInfo twState " + state);
@@ -162,16 +178,13 @@ public class KifeActivity extends SyncthingActivity implements SyncthingService.
         return "";
     }
 
-    void updateFolderUi1(String id, String label, String path) {
-        twId.setText(id);
-        twLabel.setText(label);
-        twPath.setText(path);
-    }
-
-    void updateFolderUi2(String perc, String state, String items, String size) {
-        twPercentage.setText(perc);
-        twState.setText(state);
-        twItems.setText(items);
-        twSize.setText(size);
+    void indicate() {
+        if(indicator) {
+            indicator = false;
+            indicatorView.setBackgroundResource(R.color.green);
+        } else {
+            indicator = true;
+            indicatorView.setBackgroundResource(R.color.green2);
+        }
     }
 }
